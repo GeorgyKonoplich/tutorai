dea54803119f7edeba2a05fa5eebaa1ec1a3af28
georgy <g.konoplich@elama.ru>
2017-12-11 12:22:56 +0300

    integration with database

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,7 @@
 import os
 import re
+import requests
+import logging
 
 import inflect
 import nltk
@@ -9,31 +11,6 @@ import scipy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from keras.models import load_model
 
-
-class Bot:
-    def __init__(self, root_path):
-        nltk.download('stopwords')
-        #self.topic_model = None
-        #self.topic_model = load_model(root_path + '/static/topic_predict.model')
-        #if self.topic_model is not None:
-        #    print('topic model load')
-        self.answers, self.questions, self.t2v, self.tf_vect = pre_process(root_path)
-
-    def get_sentence(self, message):
-        vec = self.tf_vect.transform([message]).toarray()[0]
-        distance = 1.0
-        index = 0
-        for i, x in enumerate(self.answers):
-            dist = scipy.spatial.distance.cosine(vec, self.t2v[i])
-            if dist < distance:
-                distance = dist
-                index = i
-        print(self.answers[index])
-        if index < len(self.answers) - 1:
-            response = self.answers[index+1][0]
-        else:
-            response = self.answers[0][0]
-        return response
 
 def pre_process(root_path):
     classes = np.load(root_path + '/static/classes.npy').item()
@@ -98,16 +75,31 @@ def transform_digit(sentence):
     return ' '.join(changed_words)
 
 
+def get_data_from_database(input_request):
+    response = requests.get('http://tutorai-env.33zqcsby6q.us-east-1.elasticbeanstalk.com/api/messagesapi/GetLastUserChain',
+                     params={'userId': input_request['userId'],
+                             'connectionId': input_request['connectionId']})
+    return response.json()
+
+
 class Bot:
     def __init__(self, root_path):
         nltk.download('stopwords')
+        self.logger = logging.getLogger('bot')
+        self.logger.debug('init')
         #self.topic_model = None
         #self.topic_model = load_model(root_path + '/static/topic_predict.model')
         #if self.topic_model is not None:
         #    print('topic model load')
         self.answers, self.questions, self.t2v, self.tf_vect = pre_process(root_path)
 
-    def get_sentence(self, message):
+    def get_sentence(self, input_request):
+        user_data = get_data_from_database(input_request=input_request)
+        self.logger.debug('user data: {}'.format(user_data))
+
+        message = user_data[u'LastBotMessage'] + ' ' + user_data[u'LastUserMessage']
+        self.logger.debug('input message: {}'.format(message))
+
         vec = self.tf_vect.transform([message]).toarray()[0]
         distance = 1.0
         index = 0
--- tutorai.py
+++ tutorai.py
@@ -5,9 +5,9 @@ from common.log import configure_loggers_with_file
 #import graphitesend
 
 app = Flask(__name__)
-bot = Bot(app.root_path)
 configure_loggers_with_file(app.root_path)
 logger = logging.getLogger('tutor')
+bot = Bot(app.root_path)
 #g = graphitesend.init(graphite_port=3031,graphite_server='127.0.0.1')
 
 
@@ -21,7 +21,7 @@ def hello_world():
 def main():
     input_data = request.get_json(request.data)
     logger.debug('Request received: {}'.format(input_data))
-    answer = bot.get_sentence(input_data['message'])
+    answer = bot.get_sentence(input_data)
     answer['userMsgId'] = input_data['userMsgId']
     answer['connectionId'] = input_data['connectionId']
     logger.debug('Response is formed: {}'.format(answer))
