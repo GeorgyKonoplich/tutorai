0ec8e098ff106a96711edb1b1296ba169970c481
georgy <g.konoplich@elama.ru>
2017-12-22 18:39:46 +0300

    test new bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,99 +1,10 @@
-import os
-import re
 import requests
 import logging
 
-import inflect
 import nltk
-import numpy as np
 import scipy
 
-from random import shuffle
-from sklearn.feature_extraction.text import TfidfVectorizer
-from keras.models import load_model
-
-
-def pre_process(root_path):
-    classes = np.load(root_path + '/static/classes.npy').item()
-    print(classes)
-    dialog_folder = root_path + '/static/dialogs_new_format/'
-    all_sentences = []
-    targets = []
-    for txt in os.listdir(dialog_folder):
-        lines = open(dialog_folder + txt, 'r').readlines()
-        lines = list(filter(lambda x: x != '\n', lines))
-        prepared_sentences = []
-        for s in lines:
-            sentence = ''.join(x for x in s if
-                                   x.isalpha() or x.isdigit() or x == ' ' or x == '.' or x == '?' or x == '!' or x == ',' or x == '\'')
-            sentence = re.sub(' +', ' ', sentence)
-            prepared_sentences.append(sentence)
-        all_sentences.append(prepared_sentences)
-        target = ''.join([x if not x.isdigit() else '' for x in txt.split('.')[0]])
-        targets.append(target)
-
-    answers = []
-    questions = []
-    texts = []
-    for i, theme in enumerate(all_sentences):
-        words = all_sentences[i][0].split()
-        s = ' '.join([word for word in words[2:]])
-        questions.append((s, classes[targets[i]]))
-        for j, line in enumerate(all_sentences[i]):
-            if line[0] != ' ':
-                words = line.split()
-                index = int(words[0].split('.')[0])
-                line = ' '.join([word for word in words[2:]])
-            else:
-                words = line.split()
-                line = ' '.join([word for word in words[1:]])
-
-            line = transform_digit(line) #todo: think about this
-            #mean = word_averaging(w2v, line.split())
-            texts.append(line)
-            answers.append((line, classes[targets[i]], index, i))
-
-    tf_vect = TfidfVectorizer(preprocessor=None)
-    tf_vect.fit(texts)
-    text2vec = list(tf_vect.transform(texts).toarray())
-    return answers, questions, text2vec, tf_vect
-
-
-def transform_digit(sentence):
-    words = sentence.split(' ')
-    ie = inflect.engine()
-    changed_words = []
-    for x in words:
-        stop = ''
-        if x[-1] == '.' or x[-1] == '?' or x[-1] == '!' or x[-1] == ',':
-            stop = x[-1]
-            x = x[0:-1]
-        if x.isdigit():
-            x = ie.number_to_words(x)
-            x = re.sub('-', ' ', x)
-        x = x + stop
-        changed_words.append(x)
-    return ' '.join(changed_words)
-
-
-def get_data_from_database(input_request):
-    response = requests.get('http://tutorai-env.33zqcsby6q.us-east-1.elasticbeanstalk.com/api/messagesapi/GetLastUserChain',
-                     params={'userId': input_request['userId'],
-                             'connectionId': input_request['connectionId']})
-    return response.json()
-
-def choose_answer(answers, theme, index):
-    answer = []
-    for i, x in enumerate(answers):
-        if x[1] != theme:
-            continue
-        if x[2] == index:
-            answer.append(x)
-    if len(answer) > 0:
-        shuffle(answer)
-        return answer[0][0], answer[0][1], answer[0][2]
-    else:
-        return None, None, None
+from functions import pre_process, get_data_from_database, transform_digit, choose_answer
 
 
 class Bot:
@@ -140,5 +51,5 @@ class Bot:
             lasttopicnumber = self.answers[0][1]
             lastrownumber = self.answers[0][2]
         state = 0
-	r = requests.get('https://api.textgears.com/check.php', params={'text': message, 'key':'PhdFWWMyoGkzCp8q'})
+        r = requests.get('https://api.textgears.com/check.php', params={'text': message, 'key':'PhdFWWMyoGkzCp8q'})
         return {'message': response, 'errors': r.json(), 'lasttopicnumber': lasttopicnumber, 'lastrownumber': lastrownumber, 'state': state}
--- tutorai.py
+++ tutorai.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from bot.bot import Bot
 import logging
 from common.log import configure_loggers_with_file
+import aiml
+import os
 #import graphitesend
 
 app = Flask(__name__)
@@ -11,10 +13,33 @@ bot = Bot(app.root_path)
 #g = graphitesend.init(graphite_port=3031,graphite_server='127.0.0.1')
 
 
-@app.route('/init')
-def hello_world():
-    logger.debug('init')
-    return 'Hello World!'
+@app.route('/')
+def hello():
+    #logger.debug('init')
+    return render_template('chat.html')
+
+@app.route("/ask", methods=['POST'])
+def ask():
+    message = str(request.form['messageText'])
+
+    kernel = aiml.Kernel()
+
+    if os.path.isfile("bot_brain.brn"):
+        kernel.bootstrap(brainFile = "bot_brain.brn")
+    else:
+        kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+        kernel.saveBrain("bot_brain.brn")
+
+    # kernel now ready for use
+    while True:
+        if message == "quit":
+            exit()
+        elif message == "save":
+            kernel.saveBrain("bot_brain.brn")
+        else:
+            bot_response = kernel.respond(message)
+            # print bot_response
+            return jsonify({'status':'OK','answer':bot_response})
 
 
 @app.route('/get_sentence/', methods=["POST"])
