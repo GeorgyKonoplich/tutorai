2756fce22621fef71ff998a0125da7fef452213e
georgy <g.konoplich@elama.ru>
2018-01-22 12:15:14 +0300

    to be

--- bot/grammar.py
+++ bot/grammar.py
@@ -30,6 +30,53 @@ def get_tags(query):
     return nltk.pos_tag(word_tokenize(query))
 
 
+def check_to_be(word, tags, i):
+    after_word = None
+    after_word2 = None
+    before_word = None
+    if i > 0:
+        before_word = tags[i - 1][0]
+    if i + 1 < len(tags):
+        after_word = tags[i + 1][0]
+    if i + 2 < len(tags):
+        after_word2 = tags[i + 2][1]
+    if word.lower() == 'i':
+        if after_word in ['am', 'was', 'wasn\'t']:
+            if is_verb(after_word2):
+                return False
+            else:
+                return True
+        else:
+            if before_word in ['am', 'was', 'wasn\'t', 'not']:
+                return True
+            else:
+                return False
+    elif word.lower() in ['you', 'we', 'they']:
+        if after_word in ['are', 'were', 'weren\'t']:
+            if is_verb(after_word2):
+                return False
+            else:
+                return True
+        else:
+            if before_word in ['are', 'were', 'weren\'t', 'not']:
+                return True
+            else:
+                return False
+    elif word.lower() in ['he', 'she', 'it']:
+        if after_word in ['is', 'was', 'wasn\'t']:
+            if is_verb(after_word2):
+                return False
+            else:
+                return True
+        else:
+            if before_word in ['is', 'was', 'wasn\'t', 'not']:
+                return True
+            else:
+                return False
+    else:
+        return True
+
+
 def check_object_pronouns(word, before_word, before_prep):
     if word in ['me', 'him', 'her', 'us', 'them']:
         if before_word[1] == 'IN':
@@ -43,6 +90,7 @@ def check_object_pronouns(word, before_word, before_prep):
             return False
     else:
         return True
+
 
 def check_plural_form_noun(word):
     if inflect.singular_noun(word) is False:
@@ -71,8 +119,12 @@ def get_errors_plural_forn_nouns(message):
     errors = []
     position = -1
     tags = nltk.pos_tag(word_tokenize(message))
+    print(tags)
     for i, tag in enumerate(tags):
         position = message.find(tag[0], position + 1)
+        answer = check_to_be(tag[0], tags, i)
+        if not answer:
+            errors.append({'ErrorType': 'error4 (to be)', 'Position': position, 'Correct': ''})
         if is_noun(tag[1]):
             answer = check_plural_form_noun(tag[0])
             if answer != True:
