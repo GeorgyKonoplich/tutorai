248df7d268243a81c24cf0b7666005b6ea936066
georgy <g.konoplich@elama.ru>
2018-01-22 11:31:26 +0300

    object pronouns

--- bot/grammar.py
+++ bot/grammar.py
@@ -30,6 +30,20 @@ def get_tags(query):
     return nltk.pos_tag(word_tokenize(query))
 
 
+def check_object_pronouns(word, before_word, before_prep):
+    if word in ['me', 'him', 'her', 'us', 'them']:
+        if before_word[1] == 'IN':
+            if is_verb(before_prep[1]):
+                return True
+            else:
+                return False
+        elif is_verb(before_word[1]):
+            return True
+        else:
+            return False
+    else:
+        return True
+
 def check_plural_form_noun(word):
     if inflect.singular_noun(word) is False:
         return True
@@ -53,11 +67,9 @@ def check_can_permission(before_word, after_word):
         return True
 
 
-
 def get_errors_plural_forn_nouns(message):
     errors = []
     position = -1
-    words = word_tokenize(message)
     tags = nltk.pos_tag(word_tokenize(message))
     for i, tag in enumerate(tags):
         position = message.find(tag[0], position + 1)
@@ -69,12 +81,21 @@ def get_errors_plural_forn_nouns(message):
             before_word = None
             after_word = None
             if i > 0:
-                before_word = words[i-1]
-            if i+1 < len(words):
-                after_word = words[i+1]
+                before_word = tags[i-1][0]
+            if i+1 < len(tags):
+                after_word = tags[i+1][0]
             answer = check_can_permission(before_word, after_word)
             if not answer:
                 errors.append({'ErrorType': 'error2 (can/cant - permission)', 'Position': position, 'Correct': ''})
+            before_word = None
+            before_prep = None
+            if i > 0:
+                before_word = tags[i-1]
+            if i - 1 > 0:
+                before_prep = tags[i-2]
+            answer = check_object_pronouns(tag[0],before_word,before_prep)
+            if not answer:
+                errors.append({'ErrorType': 'error3 (Object Pronouns)', 'Position': position, 'Correct': ''})
 
     return errors
 
